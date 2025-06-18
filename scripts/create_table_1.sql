11f80ab5c0929cac817346eef566026839e08692
pnamilak <pranay.sqldba@gmail.com>
2025-06-18 16:26:05 +0530

    update

--- scripts/create_table_1.sql
+++ scripts/create_table_1.sql
@@ -1,1 +1,9 @@
--- Test update Wed Jun 18 08:46:21 IST 2025
+USE [TennisApp]
+GO
+
+/****** Object:  Table [dbo].[Matches]    Script Date: 18-06-2025 16:25:21 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Matches]') AND type in (N'U'))
+DROP TABLE [dbo].[Matches]
+GO
+
+
